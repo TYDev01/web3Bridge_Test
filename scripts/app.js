c5da206cb286d69f2960c1e2e9991932e6db087d
TYDev01 <anthonychinaemeru84@yahoo.com>
2025-05-05 13:24:24 +0100

    Initial commit for game logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,17 @@
-// Generate random numbers from 1 - 100
 
-let newNum = Math.floor(Math.random() * 100) + 1;
+let newNum;
 
-console.log(newNum);
+
+let startGame = document.getElementById("start")
+
+startGame.addEventListener('click', (e)=>{
+    e.preventDefault()
+    // Generate random numbers from 1 - 100
+    newNum = Math.floor(Math.random() * 100) + 1
+    console.log(newNum);
+
+
+});
 
 // Get the user numbe rinput
 let guessBtn = document.getElementById("guessButton");
@@ -11,5 +20,13 @@ guessBtn.addEventListener("click", (e)=>{
     e.preventDefault();
     let userNumber = document.getElementById("userGuess");
     let convertToNumber = parseInt(userNumber.value);
-    console.log(convertToNumber)
-})
+    console.log(convertToNumber);
+
+    if (newNum === convertToNumber) {
+        console.log("Got it");
+    }else if (convertToNumber < newNum) {
+        console.log("Number is too low")
+    } else {
+        console.log("Number is too high")
+    }
+});
