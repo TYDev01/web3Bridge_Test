3221001d85a8789411aa444f5baaf27a95e40ad1
TYDev01 <anthonychinaemeru84@yahoo.com>
2025-05-05 13:00:02 +0100

    Feature: Get user guess

--- scripts/app.js
+++ scripts/app.js
@@ -2,4 +2,14 @@
 
 let newNum = Math.floor(Math.random() * 100) + 1;
 
-console.log(newNum)
+console.log(newNum);
+
+// Get the user numbe rinput
+let guessBtn = document.getElementById("guessButton");
+
+guessBtn.addEventListener("click", (e)=>{
+    e.preventDefault();
+    let userNumber = document.getElementById("userGuess");
+    let convertToNumber = parseInt(userNumber.value);
+    console.log(convertToNumber)
+})
